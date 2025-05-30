78b7f1314a41c2e2c1cf52c808b5def6dcbff92b
yeojoonsoo02 <yeojoonsoo02@gmail.com>
2025-05-30 17:37:33 +0900

    오류 수정

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,4 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import Login from "./components/Login";
 import Signup from "./components/Signup";
 import PostList from "./components/PostList";
@@ -11,32 +10,25 @@ import ProfileEdit from "./components/ProfileEdit";
 
 const App: React.FC = () => {
   return (
-    <Router>
-      <nav style={{
-        display: "flex",
-        gap: "16px",
-        padding: "16px",
-        background: "#f6f7f9",
-        borderBottom: "1px solid #e0e0e0"
-      }}>
-        <Link to="/login">로그인</Link>
-        <Link to="/signup">회원가입</Link>
-        <Link to="/posts">게시글 목록</Link>
-        <Link to="/post/new">글쓰기</Link>
-        <Link to="/profile">프로필수정</Link>
-      </nav>
-      <Routes>
-        <Route path="/login" element={<Login />} />
-        <Route path="/signup" element={<Signup />} />
-        <Route path="/posts" element={<PostList />} />
-        <Route path="/post/:id" element={<PostDetail />} />
-        <Route path="/post/new" element={<PostForm />} />
-        <Route path="/comments" element={<CommentList />} />
-        <Route path="/comment/new" element={<CommentForm />} />
-        <Route path="/profile" element={<ProfileEdit />} />
-        <Route path="*" element={<Login />} />
-      </Routes>
-    </Router>
+    <div style={{
+      display: "flex",
+      flexDirection: "column",
+      alignItems: "center",
+      gap: "40px",
+      padding: "40px 0",
+      background: "#f6f7f9",
+      minHeight: "100vh"
+    }}>
+      <h1 style={{marginBottom: 0}}>모든 도구(컴포넌트) 한눈에 보기</h1>
+      <Login />
+      <Signup />
+      <PostList />
+      <PostDetail />
+      <PostForm />
+      <CommentList />
+      <CommentForm />
+      <ProfileEdit />
+    </div>
   );
 };
 
